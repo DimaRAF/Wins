0c48819c0763d86257f8eddea13aabd4d62abc5b
DimaRAF <d.r.abolehia@gmail.com>
2026-08-04 11:05:48 +0300

    Initial Commit

---
+++ Win2/Win2App.swift
@@ -0,0 +1,17 @@
+//
+//  Win2App.swift
+//  Win2
+//
+//  Created by Dima Rafat on 21/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct Win2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
